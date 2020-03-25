90958692ff599b3954817092133e19bc64ebdaf2
abuyako <abuyako@gmail.com>
2020-03-25 12:34:30 +0300

    declaring the const values

--- js/edit.js
+++ js/edit.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const titleElement = document.querySelector('#note-title');
+const timeElement = document.querySelector('#time-stamp');
+const bodyElement = document.querySelector('#note-body');
+const noteId = location.hash.substr(1);
+let notes = getSavedNotes();
+let note = notes.find((note) => note.id === noteId);
