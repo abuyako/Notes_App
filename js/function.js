352a584e3334f5c33d38200db6e73341c3968838
abuyako <abuyako@gmail.com>
2020-03-26 15:10:43 +0300

    reading the existing notes from the local storage

--- js/function.js
+++ js/function.js
@@ -1,2 +1,12 @@
 'use strict'
 
+// Read the existing notes from the local storage
+const getSavedNotes = () => {
+    const notesJSON = localStorage.getItem('notes');
+
+    try{
+        return notesJSON ? JSON.parse(notesJSON) : [];
+    }catch (e){
+        return [];
+    }
+}
